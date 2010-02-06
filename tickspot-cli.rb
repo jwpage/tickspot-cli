f673c7d80adb8895f48a0db5dff0308edbb2e899
Johnson Page <jwpage@gmail.com>
2010-02-07 08:50:17 +1000

    Switch to 2-space tabs

--- tickspot-cli.rb
+++ tickspot-cli.rb
@@ -1,43 +1,43 @@
-require 'rubygems'
-require 'trollop'
-require 'tickspot'
-require 'yaml'
+require "rubygems"
+require "trollop"
+require "tickspot"
+require "yaml"
 
 class TickspotCli
 
-    def initialize(args)
-        cmds = %w{check}
-        initialize_help cmds
-        @args = args
-        comm = @args.shift
-        if cmds.include? comm
-            self.send comm
-        else
-            # Force -h on invalid command.
-            ARGV.push "-h"
-            initialize_help cmds
-        end
+  def initialize(args)
+    cmds = %w{check}
+    initialize_help cmds
+    @args = args
+    comm = @args.shift
+    if cmds.include? comm
+      self.send comm
+    else
+      # Force -h on invalid command.
+      ARGV.push "-h"
+      initialize_help cmds
     end
+  end
 
-    def initialize_help(stop_cmds)
-        opts = Trollop::options do
-            banner "Tickspot CLI interface"
-            opt :config, 
-                "Path to tickspot-cli config file", 
-                :short => "-c", 
-                :default => File.expand_path('~/.tickspot-cli.yaml')
-            stop_on stop_cmds
-        end
+  def initialize_help(stop_cmds)
+    opts = Trollop::options do
+      banner "Tickspot CLI interface"
+      opt :config, 
+        "Path to tickspot-cli config file", 
+        :short => "-c", 
+        :default => File.expand_path("~/.tickspot-cli.yaml")
+      stop_on stop_cmds
     end
+  end
 
 
 private
-    def check
-        opts = Trollop::options do
-            opt :user, "User email"
-        end
-        puts 'hello world'
+  def check
+    opts = Trollop::options do
+      opt :user, "User email"
     end
+    puts "hello world"
+  end
 end
 
 TickspotCli.new(ARGV)
